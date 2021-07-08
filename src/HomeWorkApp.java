975f77d379c568f7513ebd309cac3cf645d3cbf4
Mihail Nikitin <nitinin.miha@icloud.com>
2021-07-08 17:48:51 +0300

    Task 2. Created printThreeWords() method.

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -2,4 +2,9 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
     }
+    void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 }
